3e703f063801bd492b52f9b8756746dbe4df7e33
Eduardo Sandoval <edsandoval.dev@gmail.com>
2026-03-29 14:59:59 -0600

    Se añaden los componentes de Angular Material en su módulo para posterior implementación

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+// Material Modules
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatSelectModule,
+    MatCardModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatDividerModule,
+    MatIconModule,
+    MatTabsModule,
   ]
 })
 export class MaterialModule { }
